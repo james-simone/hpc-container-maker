2ab5eb4f10315a0234a86b72223489585a6981a1
James Simone <simone@wcs.fnal.gov>
2022-02-08 16:37:20 -0600

    Updated recipe for MILC.

--- recipes/milc/milc.py
+++ recipes/milc/milc.py
@@ -1,18 +1,24 @@
 """
-MILC 7.8.1
+MILC[develop] app: su3_rhmd_hisq 
 
 Contents:
-  Ubuntu 16.04
-  CUDA version 10.2
-  GNU compilers (upstream)
-  OFED (upstream)
-  OpenMPI version 3.1.4
-  QUDA version 0.8.0
+  Ubuntu 20.04 (LTS)
+  CUDA version 11.6
+  GNU compilers (upstream; 9.3.0)
+  OFED Mellanox 5.4-1.0.3.0 (ConnectX gen 4--6)
+  OpenMPI version 4.1.2
+  QUDA version develop
 """
+
 # pylint: disable=invalid-name, undefined-variable, used-before-assignment
 # pylama: ignore=E0602
 
-gpu_arch = USERARG.get('GPU_ARCH', 'sm_60')
+# command line options
+gpu_arch = USERARG.get('GPU_ARCH', 'sm_70')
+use_ucx  = USERARG.get('ucx', None) is not None
+
+devel_image   = 'nvcr.io/nvidia/cuda:11.6.0-devel-ubuntu20.04'
+runtime_image = 'nvcr.io/nvidia/cuda:11.6.0-base-ubuntu20.04'
 
 # add docstring to Dockerfile
 Stage0 += comment(__doc__.strip(), reformat=False)
@@ -20,69 +26,128 @@ Stage0 += comment(__doc__.strip(), reformat=False)
 ###############################################################################
 # Devel stage
 ###############################################################################
-Stage0 += baseimage(image='nvcr.io/nvidia/cuda:10.2-devel-ubuntu18.04',
-                    _as='devel')
+Stage0 += baseimage(image=devel_image, _as='devel')
 
-Stage0 += gnu()
-Stage0 += cmake(eula=True)
-Stage0 += ofed()
-Stage0 += openmpi(version='3.1.4')
+# GNU compilers
+compiler = gnu()
+Stage0 += compiler
+
+# Mellanox OFED
+Stage0 += mlnx_ofed(version='5.4-1.0.3.0')
+
+# OpenMPI
+if use_ucx:
+    # UCX depends on KNEM (use latest versions as of 2022-01-22)
+    Stage0 += knem(version='1.1.4')
+    Stage0 += ucx(cuda=True, version='1.12.0')
+    pass
+
+Stage0 += openmpi(version='4.1.2',
+                  cuda=True,
+                  ucx=use_ucx, infiniband=not use_ucx,
+                  toolchain=compiler.toolchain)
+
+if not use_ucx:
+    Stage0 += shell(commands=[
+        'echo "btl_openib_allow_ib = 1" >> /usr/local/openmpi/etc/openmpi-mca-params.conf'])
+
+# cmake and git
+Stage0 += cmake(eula=True, version='3.22.2')
+Stage0 += packages(ospackages=['ca-certificates', 'git'])
+
+# build xthi
+Stage0 += generic_build(branch='master',
+                        build=['make all CC=gcc MPICC=/usr/local/openmpi/bin/mpicc', ],
+                        install=['mkdir -p /usr/local/xthi/bin',
+                                 'cp /var/tmp/xthi/xthi /var/tmp/xthi/xthi.nompi /usr/local/xthi/bin'],
+                        prefix='/usr/local/xthi',
+                        repository='https://git.ecdf.ed.ac.uk/dmckain/xthi.git')
+Stage0 += environment(variables={'PATH': '/usr/local/xthi/bin:$PATH'})
 
 # build QUDA
-Stage0 += packages(ospackages=['ca-certificates', 'git'])
 Stage0 += generic_cmake(branch='develop',
-                        cmake_opts=['-D CMAKE_BUILD_TYPE=RELEASE',
-                                    '-D QUDA_DIRAC_CLOVER=ON',
-                                    '-D QUDA_DIRAC_DOMAIN_WALL=ON',
-                                    '-D QUDA_DIRAC_STAGGERED=ON',
-                                    '-D QUDA_DIRAC_TWISTED_CLOVER=ON',
-                                    '-D QUDA_DIRAC_TWISTED_MASS=ON',
-                                    '-D QUDA_DIRAC_WILSON=ON',
-                                    '-D QUDA_FORCE_GAUGE=ON',
-                                    '-D QUDA_FORCE_HISQ=ON',
-                                    '-D QUDA_GPU_ARCH={}'.format(gpu_arch),
-                                    '-D QUDA_INTERFACE_MILC=ON',
-                                    '-D QUDA_INTERFACE_QDP=ON',
-                                    '-D QUDA_LINK_HISQ=ON',
-                                    '-D QUDA_MPI=ON'],
-                        install=False,
-                        ldconfig=True,
-                        postinstall=['cp -a /var/tmp/quda/build/* /usr/local/quda'],
-                        preconfigure=['mkdir -p /usr/local/quda'],
+                        cmake_opts=['-DCMAKE_BUILD_TYPE=RELEASE',
+                                    '-DBUILD_TESTING=ON',
+                                    '-DQUDA_BUILD_SHAREDLIB=ON',
+                                    '-DQUDA_DIRAC_CLOVER=OFF',
+                                    '-DQUDA_DIRAC_CLOVER_HASENBUSCH=OFF',
+                                    '-DQUDA_DIRAC_DOMAIN_WALL=OFF',
+                                    '-DQUDA_DIRAC_NDEG_TWISTED_CLOVER=OFF',
+                                    '-DQUDA_DIRAC_NDEG_TWISTED_MASS=OFF',
+                                    '-DQUDA_DIRAC_STAGGERED=ON',
+                                    '-DQUDA_DIRAC_TWISTED_CLOVER=OFF',
+                                    '-DQUDA_DIRAC_TWISTED_MASS=OFF',
+                                    '-DQUDA_DIRAC_WILSON=ON',
+                                    '-DQUDA_DOWNLOAD_USQCD=ON',
+                                    '-DQUDA_FORCE_GAUGE=ON',
+                                    '-DQUDA_FORCE_HISQ=ON',
+                                    '-DQUDA_GPU_ARCH={}'.format(gpu_arch),
+                                    '-DQUDA_INTERFACE_MILC=ON',
+                                    '-DQUDA_INTERFACE_QDP=ON',
+                                    '-DQUDA_MPI=OFF',
+                                    '-DQUDA_QIO=ON',
+                                    '-DQUDA_QMP=ON',
+                                    '-DQUDA_TARGET_TYPE=CUDA',
+                                ],
+                        install=True,
+                        ldconfig=False,
+                        runtime=[
+                            '/usr/local/quda',
+                            '/usr/local/cuda-11.6/targets/x86_64-linux/lib/libcublas{,Lt}.so.11.*',
+                        ],
                         prefix='/usr/local/quda',
                         repository='https://github.com/lattice/quda.git')
 
+
+Stage0 += environment(variables={
+    'PATH': '/usr/local/quda/bin:$PATH',
+    'LD_LIBRARY_PATH': '/usr/local/quda/lib:$LD_LIBRARY_PATH', })
+
 # build MILC
-Stage0 += generic_build(branch='develop',
-                        build=['cp Makefile ks_imp_rhmc',
-                               'cd ks_imp_rhmc',
-                               'make -j 1 su3_rhmd_hisq \
-                                CC=/usr/local/openmpi/bin/mpicc \
-                                LD=/usr/local/openmpi/bin/mpicxx \
-                                QUDA_HOME=/usr/local/quda \
-                                WANTQUDA=true \
-                                WANT_GPU=true \
-                                WANT_CL_BCG_GPU=true \
-                                WANT_FN_CG_GPU=true \
-                                WANT_FL_GPU=true \
-                                WANT_FF_GPU=true \
-                                WANT_GF_GPU=true \
-                                MPP=true \
-                                PRECISION=2 \
-                                WANTQIO=""'],
-                        install=['mkdir -p /usr/local/milc/bin',
-                                 'cp /var/tmp/milc_qcd/ks_imp_rhmc/su3_rhmd_hisq /usr/local/milc/bin'],
-                        prefix='/usr/local/milc',
-                        repository='https://github.com/milc-qcd/milc_qcd')
-Stage0 += environment(variables={'PATH': '/usr/local/milc/bin:$PATH'})
+if True:
+    milc_opts = [
+        'PRECISION=2',
+        'OMP=true',
+        'MPP=true',
+        'CC=/usr/local/openmpi/bin/mpicc',
+        'CXX=/usr/local/openmpi/bin/mpicxx',
+        'LD=/usr/local/openmpi/bin/mpicxx',
+        'QUDA_HOME=/usr/local/quda',
+        'LD_FLAGS="-L/usr/local/cuda/lib64 -Wl,-rpath=/usr/local/cuda/lib64"',
+        'WANTQUDA=true',
+        'WANT_MIXED_PRECISION_GPU=1',
+        'WANT_CL_BCG_GPU=true',
+        'WANT_FN_CG_GPU=true',
+        'WANT_FL_GPU=true',
+        'WANT_FF_GPU=true',
+        'WANT_GF_GPU=true',
+        'WANTQMP=true',
+        'WANTQIO=true',
+        'QMPPAR=/usr/local/quda',
+        'QIOPAR=/usr/local/quda',
+    ]
+
+    Stage0 += generic_build(branch='develop',
+                            build=['cp Makefile ks_imp_rhmc',
+                                   'cd ks_imp_rhmc',
+                                   'make -j 1 su3_rhmd_hisq ' + ' '.join(milc_opts), ],
+                            install=['mkdir -p /usr/local/milc/bin',
+                                     'cp /var/tmp/milc_qcd/ks_imp_rhmc/su3_rhmd_hisq /usr/local/milc/bin'],
+                            prefix='/usr/local/milc',
+                            repository='https://github.com/milc-qcd/milc_qcd')
+    Stage0 += environment(variables={'PATH': '/usr/local/milc/bin:$PATH'})
+    pass
 
 ###############################################################################
 # Release stage
 ###############################################################################
-Stage1 += baseimage(image='nvcr.io/nvidia/cuda:10.2-base-ubuntu18.04')
-
-Stage1 += packages(ospackages=['libcublas10'])
+Stage1 += baseimage(image=runtime_image)
 
 Stage1 += Stage0.runtime()
 
-Stage1 += environment(variables={'PATH': '/usr/local/milc/bin:$PATH'})
+# libnuma.so.1 needed by xthi
+Stage1 += packages(apt=['libnuma1'],yum=['numactl-libs',])
+
+Stage1 += environment(variables={
+    'PATH': '/usr/local/milc/bin:/usr/local/quda/bin:/usr/local/xthi/bin:$PATH',
+    'LD_LIBRARY_PATH': '/usr/local/quda/lib:/usr/local/cuda/lib64:$LD_LIBRARY_PATH', })
